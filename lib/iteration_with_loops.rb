446d3acdfd2d4dcf6304eac26dc6cb6b7b45b3e2
Luca Kanyo <lucakanyo@protonmail.com>
2020-03-31 00:10:48 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -4,7 +4,8 @@ def join_nested_strings(src)
     column_count = 0 
     sentence = ""
     while column_count < src[row_count].length do 
-      if src[row_count][column_count]
+      if type(src[row_count][column_count]) == String
+        sentence.push()
       column_count += 1
     end
     row_count += 1 
